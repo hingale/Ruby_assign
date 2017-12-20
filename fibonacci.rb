7c305a43cdad1723fa945a9ed6b092183e5b766e
Manoj Kulkarni <Haribhau.Ingale@synechron.com>
2017-12-20 10:55:30 +0530

    modified fibonacci program

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,7 +1,7 @@
 # Program to calculate fibonacci series using ruby for given number
 # author: Yogesh 
 
-
+#solution 1
 puts "Enter the Number"
 n=gets.to_i
 a=0
@@ -16,8 +16,7 @@ b=sum
 end
 
 
-=begin
-
+#solution 2
 value = 10
 a = 0
 b = 1
@@ -28,10 +27,10 @@ a = b
 b = a + b
 end
 
-=end
 
 
-=begin
+
+#solution 3
 class Fibo
 def get_value
 n=gets.chomp.to_i
@@ -52,7 +51,7 @@ end
  d=Fibo.new
  d.get_value
 
-=end
+
 
 
 
